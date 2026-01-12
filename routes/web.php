93fe53ac7203d318466c35ffa91a96ce5e2211b9
Devan Erza <devanersa6@gmail.com>
2026-01-12 13:13:05 +0700

    feat: added shadcn for UI polish, added tanstack table component and dependencies

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,27 @@
 <?php
 
-use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/applications', [ApplicationController::class, 'index'])->name('applications.index');
-    Route::get('/applications/create', [ApplicationController::class, 'create'])->name('applications.create');
-    Route::get('/applications/{application}/edit', [ApplicationController::class, 'edit'])->name('applications.edit');
-    Route::post('/applications', [ApplicationController::class, 'store'])->name('applications.store');
-    Route::delete('/applications/{application}', [ApplicationController::class, 'destroy'])->name('applications.destroy');
-    Route::put('/applications/{application}', [ApplicationController::class, 'update'])->name('applications.update');
-    Route::match(['PATCH', 'PUT'], '/applications/{application}/status', [ApplicationController::class, 'statusUpdate'])->name('applications.statusUpdate');
-    Route::match(['PATCH', 'PUT'], '/applications/{application}/follow_up_at', [ApplicationController::class, 'followUp'])->name('applications.followUp');
 });
 
 require __DIR__.'/auth.php';
