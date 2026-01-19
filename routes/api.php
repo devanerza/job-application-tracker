2ffcaaedc310af9c90e1607ac4e019a0db28b8c1
Devan Erza <devanersa6@gmail.com>
2026-01-19 17:48:21 +0700

    fix: resolved incorrect /test api route definition

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 
-Route::get('/test' function() {
+Route::get('/test', function() {
     return "API route Read successfully";
 });
 
