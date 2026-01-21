4c1b83179c4c53e3731fac5127c2646fa8744f88
Devan Erza <devanersa6@gmail.com>
2026-01-21 16:22:51 +0700

    refactor: change api route to a proper installation using artisan install:api

--- routes/api.php
+++ routes/api.php
@@ -1,36 +1,8 @@
 <?php
 
-use App\Models\Application;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
-
-// A basic test route that uses no external classes
-Route::get('/test-connection', function () {
-    return response()->json(['status' => 'Laravel is alive on Vercel']);
-});
-
-// Your Cron Route
-
-Route::get('/cron/reminders', function () {
-    return response()->json(['message' => 'The route is definitely working!']);
-});
-
-// Route::get('/cron/reminders', function (Request $request) {
-//     $secret = env('CRON_SECRET');
-    
-//     if (!$request->has('key') || $request->query('key') !== $secret) {
-//         return response()->json(['error' => 'Unauthorized'], 403);
-//     }
-
-//     try {
-//         Artisan::call('reminders:send');
-//         return response()->json([
-//             'status' => 'success',
-//             'output' => Artisan::output()
-//         ]);
-//     } catch (\Exception $e) {
-//         return response()->json(['error' => $e->getMessage()], 500);
-//     }
-// });
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
