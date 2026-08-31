0246ea6c5d7548dd04d3636a8746dfbe0cd1142b
Devan Erza <devanersa6@gmail.com>
2026-08-31 13:20:54 +0700

    clean up routes

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ApplicationController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
 Route::get('/dashboard', [ApplicationController::class, 'index'])
 ->middleware(['auth', 'verified'])
